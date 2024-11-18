5beb1ba0afb1e42c0ddb0cb33f3d660f9818ecf0
sivalakshmi07 <sivalakshmips37@gmail.com>
2024-11-18 16:05:32 +0530

    Add files via upload

---
+++ sumof2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int x,y,sum;
+	printf("enter the first number:");
+	scanf("%d",&x);
+	printf("enter the second number:");
+	scanf("%d",&y);
+	sum=x+y;
+	printf("the sum of%d and %d is:%d",x,y,sum);
+	return 0;
+	}
